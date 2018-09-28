6a55aaad185a98f156cd11c31f9f6fee0cff3b87
sherm247 <sherm247@msu.edu>
2018-09-28 15:35:30 -0400

    Initial Commit

---
+++ Anomaly_Dection_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anomaly_Dection_iOS
+//
+//  Created by CSE498 on 9/26/18.
+//  Copyright © 2018 CSE498. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
