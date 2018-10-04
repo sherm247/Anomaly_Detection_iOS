11aca880ecc423b72c45850a1abbecd5ab061352
sherm247 <sherm247@msu.edu>
2018-10-04 19:17:42 -0400

    Complete Commit

--- Anomaly_Dection_iOS/ViewController.swift
+++ Anomaly_Dection_iOS/ViewController.swift
@@ -8,8 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Type...................Credit Card",
+                "Transaction Amount.....-$31.42",
+                "Posted Date............Oct 2, 2018",
+                "Transaction Date.......Oct 1, 2018",
+                "Card Number............x1234",
+                "New Balance............$271.83",
+                "Description............DARTH PLAGUEIS W., SAN FRANCISCO, CA",
+                "Warnings...............POTENTIAL FRAUDULENT TRANSACTION"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
+    //MARK: Properties
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet var White: [UIImageView]!
+    @IBOutlet var MSUFCU: [UILabel]!
+    @IBOutlet weak var ProfileButton: UIButton!
+    @IBOutlet weak var TransactionsLabel: UILabel!
+    @IBOutlet weak var WhiteBar2: UIImageView!
+    @IBOutlet weak var AccountsImg: UIImageView!
+    @IBOutlet weak var MoveMoneyImg: UIImageView!
+    @IBOutlet weak var eDepositImg: UIImageView!
+    @IBOutlet weak var MyOffersImg: UIImageView!
+    @IBOutlet weak var AccountsLabel: UILabel!
+    @IBOutlet weak var MoveMoneyLabel: UILabel!
+    @IBOutlet weak var eDepositLabel: UILabel!
+    @IBOutlet weak var MyOffersLabel: UILabel!
+    @IBOutlet weak var EmbeddedTable: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
