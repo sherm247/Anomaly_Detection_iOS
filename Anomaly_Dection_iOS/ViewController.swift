38e97acdd8a69ff57d1d791a821f66e3dff0568c
Team MSUFCU <sherm247@msu.edu>
2018-10-26 23:59:16 -0400

    New table view. Re-establish connection to database

--- Anomaly_Dection_iOS/ViewController.swift
+++ Anomaly_Dection_iOS/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 //import SwiftyJSON
 //import Alamofire
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController { //, UITableViewDelegate, UITableViewDataSource {
     
     let list = ["Type       Credit Card",
                 "Transaction Amount     -$31.42",
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 "Description        WALMART 3452 CUSCO",
                 "Warnings       POTENTIAL FRAUD"]
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return(list.count)
     }
     
@@ -29,10 +29,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         //let cell = UITableViewCell(style: <#T##UITableViewCellStyle#>, reuseIdentifier: "Cell")
         cell.textLabel?.text = list[indexPath.row]
         return(cell)
-    }
+    }*/
     
     
+    @IBOutlet weak var tableView: UITableView!
     
+    var transactions:[(cardAcceptor:String, postAmount:String, date:String,
+    cardNum:String, fraudFlag:String)]
+    
+    private var cardAcceptorValue:[String] = []
+    private var postAmountValue:[String] = []
+    private var dateValue:[String] = []
+    private var cardNumValue:[String] = []
+    private var fraudFlagValue:[String] = []
     
     //MARK: Properties
     @IBOutlet weak var nameText: UILabel!
@@ -54,7 +63,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        struct Account: Decodable {
+        /*struct Account: Decodable {
             let transactions: [Transaction]
             
             enum CodingKeys : String, CodingKey {
@@ -70,15 +79,92 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let card_acceptor_city: String
             let card_acceptor_country: String
             let fraud_flag: Bool
-        }
+        }*/
         
         menuView.layer.shadowOpacity = 1
         menuView.layer.shadowRadius = 6
         
+        callDtaFromWeb()
     }
     
+    func callDtaFromWeb(){
+        let request = NSMutableURLRequest(URL: NSURL(string:"http://django-env.zqqwi3vey2.us-east-1.elasticbeanstalk.com/api/transactions/?account=3243617280")!)
+        httpGet(request){
+            (data, error) -> Void in
+            if error != nil {
+                print(error)
+            }else {
+                print(data) // Print all data to console
+                
+                // Might not work for database
+                let delimiter = ":"
+                
+                self.items = []
+                let lines:[String] = data.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet ()) as [String]
+                
+                for line in lines {
+                    var values:[String] = []
+                    if line != "" {
+                        values = line.componentsSeparatedByString(delimiter)
+                        // Put the values into the tuple and add it to the items array
+                        self.post_amount.append(values[1])
+                        self.card_acceptor_name.append(values[0])
+                        self.our_transmission_date.append(values[2])
+                        self.processor_account.append(values[3])
+                        self.fraud_flag.append(values[4])
+                        
+                        let transaction = (description: values[0], amount: values[1], date: values[2], cardNum: values[3], fraudFlag: values[4])
+                        self.transactions?.append(transaction)
+                    }
+                }
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                    
+                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                })
+            }
+        }
+        
+    }
     
-    func getData() -> Array<Any> {
+    func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) {
+            (data,response,error) -> Void in
+            if error != nil {
+                callback("",error!.localizedDescription)
+            }else {
+                let result = NSString(data: data!, encoding: NSASCIIStringEncoding)!
+                callback(result as String, nil)
+            }
+        }
+        task.resume()
+    }
+    
+    func numberOfSecionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        // Return the number of rows in the section.
+        
+        return postAmountValue.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                print("stuff")
+            
+            cell.textLabel?.text = self.cardAcceptorValue[indexPath.row];
+            cell.detailTextLabel?.text = self.postAmountValue[indexPath.row];
+        })
+        return cell
+    }
+    
+    /*func getData() -> Array<Any> {
         
         let urlString = URL(string: "http://django-env.zqqwi3vey2.us-east-1.elasticbeanstalk.com/api/transactions/?account=3243617280")
         
@@ -108,7 +194,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                                 
                                 temp_list = [type, amount, date, card_number, description, fraud_flag] //as [Any]
                                 
-                                func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                                /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
                                     
                                     return(temp_list.count)
                                 }
@@ -120,7 +206,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                                         //let cell = UITableViewCell(style: <#T##UITableViewCellStyle#>, reuseIdentifier: "Cell")
                                         cell.textLabel?.text = temp_list[indexPath.row] as? String
                                         return(cell)
-                                }
+                                }*/
                                 
                                 //list_of_lists += list
                                 
@@ -154,7 +240,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         return temp_list
         
-    }
+    }*/
     
     
     @IBAction func openMenu(_ sender: Any) {
