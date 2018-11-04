2464269f4f715ec70541e763a0e2771b3ae4041f
sherm247 <sherm247@msu.edu>
2018-11-04 00:41:18 -0400

    Table displays data from database. When you click on a cell, a pop up displays with more information

--- Anomaly_Dection_iOS/ViewController.swift
+++ Anomaly_Dection_iOS/ViewController.swift
@@ -10,132 +10,111 @@ import UIKit
 //import SwiftyJSON
 //import Alamofire
 
-class ViewController: UIViewController { //, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let list = ["Type       Credit Card",
-                "Transaction Amount     -$31.42",
-                "Posted Date        Oct 2, 2018",
-                "Transaction Date       Oct 1, 2018",
-                "Card Number        x1234",
-                "Description        WALMART 3452 CUSCO",
-                "Warnings       POTENTIAL FRAUD"]
+    var transactions:[(cardAcceptor:Any, postAmount:Any, date:Any,
+        cardNum:Any, fraudFlag:Any)]?
     
-    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return(list.count)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
-        //let cell = UITableViewCell(style: <#T##UITableViewCellStyle#>, reuseIdentifier: "Cell")
-        cell.textLabel?.text = list[indexPath.row]
-        return(cell)
-    }*/
-    
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    var transactions:[(cardAcceptor:String, postAmount:String, date:String,
-    cardNum:String, fraudFlag:String)]
-    
-    private var cardAcceptorValue:[String] = []
-    private var postAmountValue:[String] = []
-    private var dateValue:[String] = []
-    private var cardNumValue:[String] = []
-    private var fraudFlagValue:[String] = []
-    
-    //MARK: Properties
-    @IBOutlet weak var nameText: UILabel!
-    @IBOutlet var White: [UIImageView]!
-    @IBOutlet var MSUFCU: [UILabel]!
-    @IBOutlet weak var TransactionsLabel: UILabel!
-    @IBOutlet weak var AccountsLabel: UILabel!
-    @IBOutlet weak var MoveMoneyLabel: UILabel!
-    @IBOutlet weak var eDepositLabel: UILabel!
-    @IBOutlet weak var MyOffersLabel: UILabel!
-    @IBOutlet weak var EmbeddedTable: UIView!
+    private var cardAcceptorValue:[Any] = []
+    private var postAmountValue:[Any] = []
+    private var dateValue:[Any] = []
+    private var cardNumValue:[Any] = []
+    private var fraudFlagValue:[Any] = []
     
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var leadingContraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var transactionTable: UITableView!
     
     var menuShowing = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //transactionTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+ 
         
-        /*struct Account: Decodable {
-            let transactions: [Transaction]
-            
-            enum CodingKeys : String, CodingKey {
-                case transactions
-            }
-        }
-        
-        struct Transaction: Decodable {
-            let id: String
-            let processor_account: String
-            let post_amount: String
-            let card_acceptor_name: String
-            let card_acceptor_city: String
-            let card_acceptor_country: String
-            let fraud_flag: Bool
-        }*/
-        
-        menuView.layer.shadowOpacity = 1
-        menuView.layer.shadowRadius = 6
+        //menuView.layer.shadowOpacity = 1
+        //menuView.layer.shadowRadius = 6
         
         callDtaFromWeb()
     }
     
     func callDtaFromWeb(){
-        let request = NSMutableURLRequest(URL: NSURL(string:"http://django-env.zqqwi3vey2.us-east-1.elasticbeanstalk.com/api/transactions/?account=3243617280")!)
-        httpGet(request){
+        let request = URLRequest(url: URL(string:"http://django-env.zqqwi3vey2.us-east-1.elasticbeanstalk.com/api/transactions/?account=3438561154")!)
+        httpGet(request: request){
             (data, error) -> Void in
             if error != nil {
+                print("http error")
                 print(error)
             }else {
-                print(data) // Print all data to console
+                //print(data) // Print all data to console
                 
-                // Might not work for database
-                let delimiter = ":"
-                
-                self.items = []
-                let lines:[String] = data.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet ()) as [String]
-                
-                for line in lines {
-                    var values:[String] = []
-                    if line != "" {
-                        values = line.componentsSeparatedByString(delimiter)
-                        // Put the values into the tuple and add it to the items array
-                        self.post_amount.append(values[1])
-                        self.card_acceptor_name.append(values[0])
-                        self.our_transmission_date.append(values[2])
-                        self.processor_account.append(values[3])
-                        self.fraud_flag.append(values[4])
+                if let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [[String: Any]] {
+                    
+                    print(json?.count)
+                    
+                    var x = 0
+                    
+                    for transaction in json!{
+                        print(transaction["post_amount"]!)
+                        self.postAmountValue.append(String(format: "%.2f",transaction["post_amount"] as! float_t))
+                        print(self.postAmountValue[0])
+                        self.cardAcceptorValue.append(transaction["card_acceptor_name"]!)
+                        self.dateValue.append(transaction["our_transmission_date"]!)
+                        self.cardNumValue.append(transaction["processor_account"]!)
+                        self.fraudFlagValue.append(transaction["fraud_flag"]!)
                         
-                        let transaction = (description: values[0], amount: values[1], date: values[2], cardNum: values[3], fraudFlag: values[4])
-                        self.transactions?.append(transaction)
+                        let trans = (self.postAmountValue[x], self.cardAcceptorValue[x], self.dateValue[x], self.cardNumValue[x], self.fraudFlagValue[x])
+                        self.transactions?.append(trans)
+                        
+                        x += 1
                     }
                 }
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.tableView.reloadData()
-                    
-                    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-                })
+                
             }
+            
+            self.postAmountValue.append("40.00")
+            self.cardAcceptorValue.append("MSUFCU")
+            self.dateValue.append("November 2, 2018")
+            self.cardNumValue.append("1234567")
+            self.fraudFlagValue.append("0")
+            
+            self.postAmountValue.append("167.19")
+            self.cardAcceptorValue.append("WALMART")
+            self.dateValue.append("November 1, 2018")
+            self.cardNumValue.append("1234567")
+            self.fraudFlagValue.append("3")
+            
+            self.postAmountValue.append("19.95")
+            self.cardAcceptorValue.append("PLANET FITNESS")
+            self.dateValue.append("November 3, 2018")
+            self.cardNumValue.append("1234567")
+            self.fraudFlagValue.append("1")
+            
+            self.postAmountValue.append("567.32")
+            self.cardAcceptorValue.append("MEIJER")
+            self.dateValue.append("November 2, 2018")
+            self.cardNumValue.append("1234567")
+            self.fraudFlagValue.append("2")
+            
+            DispatchQueue.main.async {
+                self.transactionTable.reloadData()
+            }
+            //self.transactionTable.reloadData()
         }
-        
     }
     
-    func httpGet(request: NSURLRequest!, callback: (String, String?) -> Void) {
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) {
+    func httpGet(request: URLRequest!, callback: @escaping (Data?, String?) -> Void) {
+        print("inside httpget")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {
             (data,response,error) -> Void in
             if error != nil {
-                callback("",error!.localizedDescription)
+                callback(nil,error!.localizedDescription)
             }else {
-                let result = NSString(data: data!, encoding: NSASCIIStringEncoding)!
-                callback(result as String, nil)
+                //let result = String(data: data!, encoding: String.Encoding(rawValue: String.Encoding.ascii.rawValue))!
+                //print(result as String)
+                callback(data, nil)
             }
         }
         task.resume()
@@ -146,104 +125,70 @@ class ViewController: UIViewController { //, UITableViewDelegate, UITableViewDat
         return 1
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         // Return the number of rows in the section.
         
-        return postAmountValue.count
+        return cardAcceptorValue.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as UITableViewCell
         
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        let cellIdentifier = "cell"
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .value1, reuseIdentifier: cellIdentifier)
+        }
+        
+        /*dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 print("stuff")
             
             cell.textLabel?.text = self.cardAcceptorValue[indexPath.row];
             cell.detailTextLabel?.text = self.postAmountValue[indexPath.row];
-        })
-        return cell
+        })*/
+        
+        /*for transaction in transactions!{
+            print(transaction.postAmount)
+        }*/
+        
+        cell?.textLabel?.text = self.cardAcceptorValue[indexPath.row] as? String;
+        print(self.cardAcceptorValue[0] as? String)
+        cell?.detailTextLabel?.text = self.postAmountValue[indexPath.row] as? String;
+        print(self.postAmountValue[0] as? String)
+    
+        //cell.textLabel?.text = "Walmart";
+        //cell.detailTextLabel?.text = "-$35.33";
+        
+        return cell!
     }
     
-    /*func getData() -> Array<Any> {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let urlString = URL(string: "http://django-env.zqqwi3vey2.us-east-1.elasticbeanstalk.com/api/transactions/?account=3243617280")
+        let cardAcceptor = self.cardAcceptorValue[indexPath.row] as! String
+        let postAmount = self.cardAcceptorValue[indexPath.row] as! String
+        let date = self.dateValue[indexPath.row] as! String
+        let cardNum = self.cardNumValue[indexPath.row] as! String
+        var fraudFlag = self.fraudFlagValue[indexPath.row] as! String
         
-        //var list_of_lists = Array<Any>()
-        
-        var temp_list = Array<Any>()
-    
-        if let url = urlString {
-            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    print(error!)
-                } else {
-                    if let usableData = data {
-                        //print(usableData) //JSONSerialization
-                        if let json = try? JSONSerialization.jsonObject(with: usableData, options: []) as? [[String: Any]] {
-                            
-                            for transaction in json!{
-                                //print(transaction)
-                                //print(transaction["id"]!)
-                                //let trans_id = transaction["id"] as? String
-                                let type = transaction["tran_sub_type"] as? Int
-                                let amount = transaction["post_amount"] as? Float
-                                let date = transaction["our_transmission_date"] as? String
-                                let card_number = transaction["processor_account"] as? String
-                                let description = transaction["card_acceptor_name"] as? String
-                                let fraud_flag = transaction["fraud_flag"] as? Int
-                                
-                                temp_list = [type, amount, date, card_number, description, fraud_flag] //as [Any]
-                                
-                                /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                                    
-                                    return(temp_list.count)
-                                }
-                                
-                                func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-                                    UITableViewCell {
-                                        
-                                        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
-                                        //let cell = UITableViewCell(style: <#T##UITableViewCellStyle#>, reuseIdentifier: "Cell")
-                                        cell.textLabel?.text = temp_list[indexPath.row] as? String
-                                        return(cell)
-                                }*/
-                                
-                                //list_of_lists += list
-                                
-                                /*for item in temp_list{
-                                    print(item)
-                                }*/
-                                
-                                
-                                //print(transaction["fraud_flag"]!)
-                                /*let fraud_flag = "1"
-                                 if (fraud_flag as Any? === transaction["fraud_flag"]){
-                                 print("POTENTIAL FRAUD")
-                                 }*/
-                            }
-                            //return temp_list
-                        }
-                        /*for item in temp_list{
-                            print(item)
-                        }*/
-                        print("able to access data")
-                    }
-                }
-                for item in temp_list{
-                    print(item)
-                }
-                
-            }
-            task.resume()
+        if fraudFlag == "0"{
+            fraudFlag = "None"
+        }else if fraudFlag == "1"{
+            fraudFlag = "THIS RECURRING PAYMENT HAS INCREASED"
+        }else if fraudFlag == "2"{
+            fraudFlag = "THIS PAYMENT IS POTENTIALLY FRAUDULENT"
+        }else if fraudFlag == "3"{
+            fraudFlag = "THIS PAYMENT IS LIKELY FRAUDULENT"
         }
-        //print("test")
         
-        return temp_list
-        
-    }*/
+        let msg = "Description: " + cardAcceptor + " \n " + "Amount: " + postAmount + " \n " + "Date: " + date + " \n " + "Card Number: " + cardNum + " \n " + "Warnings: " + fraudFlag
+        let alert = UIAlertController(title: "Transaction Details", message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
+    }
     
     
-    @IBAction func openMenu(_ sender: Any) {
+    /*@IBAction func openMenu(_ sender: Any) {
         
         if (menuShowing) {
             leadingContraint.constant = -150
@@ -257,7 +202,7 @@ class ViewController: UIViewController { //, UITableViewDelegate, UITableViewDat
         }
         
         menuShowing = !menuShowing
-    }
+    }*/
 
 
 }
