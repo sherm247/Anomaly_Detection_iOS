869f52ea17d82cf005573613d1990452b501b571
sherm247 <sherm247@msu.edu>
2018-11-12 16:15:23 -0500

    Beta Version: Feature Complete

--- Anomaly_Dection_iOS/ViewController.swift
+++ Anomaly_Dection_iOS/ViewController.swift
@@ -12,8 +12,13 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var transactions:[(cardAcceptor:String, postAmount:NSNumber, date:String,
-    cardNum:String, fraudFlag:NSNumber, cardAcceptorCity:String, cardAcceptorState:String, cardAcceptorCountry:String)]?
+    var transactions = [(cardAcceptor:String, postAmount:NSNumber, date:String, cardNum:String, fraudFlag:NSNumber, cardAcceptorCity:String, cardAcceptorState:String, cardAcceptorCountry:String)]()
+    
+    var card1 = [(cardAcceptor:String, postAmount:NSNumber, date:String, cardNum:String, fraudFlag:NSNumber, cardAcceptorCity:String, cardAcceptorState:String, cardAcceptorCountry:String)]()
+    
+    var card2 = [(cardAcceptor:String, postAmount:NSNumber, date:String, cardNum:String, fraudFlag:NSNumber, cardAcceptorCity:String, cardAcceptorState:String, cardAcceptorCountry:String)]()
+    
+    var CARD_TO_USE = "CARD1"
     
     private var cardAcceptorValue:[String] = []
     private var postAmountValue:[NSNumber] = []
@@ -36,19 +41,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         //transactionTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
  
         
-        //menuView.layer.shadowOpacity = 1
-        //menuView.layer.shadowRadius = 6
+        menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowRadius = 6
+    
+        debitCardButtonVar.setTitleColor(UIColor.darkGray, for: [])
+        creditCardButtonVar.setTitleColor(UIColor.blue, for: [])
+        
+        activityIndicator()
         
         callDtaFromWeb()
+        
+        self.transactionTable.addSubview(self.refreshControl)
+        
+        self.transactionTable.tableFooterView = UIView(frame:CGRect.zero)
     }
     
     func callDtaFromWeb(){
         let request = URLRequest(url: URL(string:"http://django-env.zqqwi3vey2.us-east-1.elasticbeanstalk.com/api/transactions/?account=1387654812")!)
+        indicator.startAnimating()
+        indicator.backgroundColor = UIColor.black
         httpGet(request: request){
             (data, error) -> Void in
             if error != nil {
                 print("http error")
-                print(error)
+                print(error!)
             }else {
                 //print(data) // Print all data to console
                 
@@ -58,6 +74,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     
                     var x = 0
                     
+                    self.postAmountValue.removeAll()
+                    self.cardAcceptorValue.removeAll()
+                    self.dateValue.removeAll()
+                    self.fraudFlagValue.removeAll()
+                    self.cardAcceptorCityValue.removeAll()
+                    self.cardAcceptorStateValue.removeAll()
+                    self.cardAcceptorCountryValue.removeAll()
+                    self.transactions.removeAll()
+                    
+                    
                     for transaction in json!{
                         //print(transaction["post_amount"]!)
                         self.postAmountValue.append(transaction["post_amount"]! as! NSNumber)
@@ -66,13 +92,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                         self.cardAcceptorValue.append(transaction["card_acceptor_name"]! as! String)
                         self.dateValue.append(transaction["our_transmission_date"]! as! String)
                         self.cardNumValue.append(transaction["processor_account"]! as! String)
+                        //print(self.cardNumValue[x])
                         self.fraudFlagValue.append(transaction["fraud_flag"]! as! NSNumber)
                         self.cardAcceptorCityValue.append(transaction["card_acceptor_city"]! as! String)
                         self.cardAcceptorStateValue.append(transaction["card_acceptor_state"]! as! String)
                         self.cardAcceptorCountryValue.append(transaction["card_acceptor_country"]! as! String)
                         
                         let trans = (self.cardAcceptorValue[x], self.postAmountValue[x], self.dateValue[x], self.cardNumValue[x], self.fraudFlagValue[x], self.cardAcceptorCityValue[x], self.cardAcceptorStateValue[x], self.cardAcceptorCountryValue[x])
-                        self.transactions?.append(trans)
+                        //print(trans)
+                        //self.transactions.append(trans)
+                        
+                        //print(trans.3)
+                        
+                        if trans.3 == "4888887992597070"{
+                            self.card1.append(trans)
+                            if self.CARD_TO_USE == "CARD1"{
+                                //self.debitCardButtonVar.setTitleColor(UIColor.darkGray, for: [])
+                                self.transactions.append(trans)
+                            }
+                        }else if trans.3 == "4888889523448350"{
+                            self.card2.append(trans)
+                            if self.CARD_TO_USE == "CARD2"{
+                                //self.creditCardButtonVar.setTitleColor(UIColor.darkGray, for: [])
+                                self.transactions.append(trans)
+                            }
+                        }
                         
                         x += 1
                     }
@@ -81,43 +125,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
             
             /*var indx = 0
-            for transaction in self.transactions!{
+            for transaction in self.transactions{
                 if transaction.fraudFlag != 0{
-                    self.transactions?.remove(at: indx)
-                    self.transactions?.insert(transaction, at: 0)
+                    self.transactions.remove(at: indx)
+                    self.transactions.insert(transaction, at: 0)
+                }
+                indx += 1
+            }
+            indx = 0
+            for transaction in self.card1{
+                if transaction.fraudFlag != 0{
+                    self.card1.remove(at: indx)
+                    self.card1.insert(transaction, at: 0)
+                }
+                indx += 1
+            }
+            indx = 0
+            for transaction in self.card2{
+                if transaction.fraudFlag != 0{
+                    self.card2.remove(at: indx)
+                    self.card2.insert(transaction, at: 0)
                 }
                 indx += 1
             }*/
             
-            /*self.postAmountValue.append("40.00")
-            self.cardAcceptorValue.append("MSUFCU")
-            self.dateValue.append("November 2, 2018")
-            self.cardNumValue.append("1234567")
-            self.fraudFlagValue.append("0")
-            
-            self.postAmountValue.append("167.19")
-            self.cardAcceptorValue.append("WALMART")
-            self.dateValue.append("November 1, 2018")
-            self.cardNumValue.append("1234567")
-            self.fraudFlagValue.append("3")
-            
-            self.postAmountValue.append("19.95")
-            self.cardAcceptorValue.append("PLANET FITNESS")
-            self.dateValue.append("November 3, 2018")
-            self.cardNumValue.append("1234567")
-            self.fraudFlagValue.append("1")
-            
-            self.postAmountValue.append("567.32")
-            self.cardAcceptorValue.append("MEIJER")
-            self.dateValue.append("November 2, 2018")
-            self.cardNumValue.append("1234567")
-            self.fraudFlagValue.append("2")*/
-            
             DispatchQueue.main.async {
                 self.transactionTable.reloadData()
             }
+            
             //self.transactionTable.reloadData()
         }
+        indicator.stopAnimating()
+        indicator.hidesWhenStopped = true
     }
     
     func httpGet(request: URLRequest!, callback: @escaping (Data?, String?) -> Void) {
@@ -144,7 +183,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         // Return the number of rows in the section.
         
-        return cardAcceptorValue.count
+        return transactions.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -157,33 +196,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             cell = UITableViewCell(style: .value1, reuseIdentifier: cellIdentifier)
         }
         
-        /*dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                print("stuff")
-            
-            cell.textLabel?.text = self.cardAcceptorValue[indexPath.row];
-            cell.detailTextLabel?.text = self.postAmountValue[indexPath.row];
-        })*/
         
-        /*for transaction in transactions!{
-            print(transaction.postAmount)
-        }*/
-        
-        cell?.textLabel?.text = self.cardAcceptorValue[indexPath.row]
+        cell?.textLabel?.text = self.transactions[indexPath.row].cardAcceptor
         //print(self.cardAcceptorValue[0] as? String)
-        cell?.detailTextLabel?.text = String(format:"$%.2f", self.postAmountValue[indexPath.row].doubleValue)
+        cell?.detailTextLabel?.text = String(format:"$%.2f", self.transactions[indexPath.row].postAmount.doubleValue)
         cell?.detailTextLabel?.textColor = UIColor.black
         //print(self.postAmountValue[0] as? String)
-    
-        //cell.textLabel?.text = "Walmart";
-        //cell.detailTextLabel?.text = "-$35.33";
         
-        if self.fraudFlagValue[indexPath.row] == 3{
-            cell?.backgroundColor = UIColor.red
-        }else if self.fraudFlagValue[indexPath.row] == 2{
-            cell?.backgroundColor = UIColor.yellow
-        }else if self.fraudFlagValue[indexPath.row] == 1{
-            cell?.backgroundColor = UIColor.orange
-        }else{
+        if self.transactions[indexPath.row].fraudFlag == 3 as NSNumber{
+            cell?.backgroundColor = UIColor(red:0.87,green:0.55,blue:0.54,alpha:1.0)
+        }else if self.transactions[indexPath.row].fraudFlag == 2 as NSNumber{
+            cell?.backgroundColor = UIColor.clear
+        }else if self.transactions[indexPath.row].fraudFlag == 1 as NSNumber{
+            cell?.backgroundColor = UIColor(red:1.00,green:0.89,blue:0.77,alpha:1.0)
+        }else if self.transactions[indexPath.row].fraudFlag == 0 as NSNumber{
             cell?.backgroundColor = UIColor.clear
         }
         
@@ -192,7 +218,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        let cardAcceptor = self.cardAcceptorValue[indexPath.row]
+        /*let cardAcceptor = self.cardAcceptorValue[indexPath.row]
         let postAmount = self.postAmountValue[indexPath.row]
         let date = self.dateValue[indexPath.row]
         let cardNum = self.cardNumValue[indexPath.row]
@@ -200,16 +226,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var _ff = ""
         let city = self.cardAcceptorCityValue[indexPath.row]
         let state = self.cardAcceptorStateValue[indexPath.row]
-        let country = self.cardAcceptorCountryValue[indexPath.row]
+        let country = self.cardAcceptorCountryValue[indexPath.row]*/
+        
+        let cardAcceptor = self.transactions[indexPath.row].cardAcceptor
+        let postAmount = self.transactions[indexPath.row].postAmount
+        let date = self.transactions[indexPath.row].date
+        let cardNum = self.transactions[indexPath.row].cardNum
+        let fraudFlag = self.transactions[indexPath.row].fraudFlag
+        var _ff = ""
+        let city = self.transactions[indexPath.row].cardAcceptorCity
+        let state = self.transactions[indexPath.row].cardAcceptorState
+        let country = self.transactions[indexPath.row].cardAcceptorCountry
+        
         let location = city + ", " + state + ", " + country
         
-        if fraudFlag == 0{
+        if fraudFlag == 0 as NSNumber{
             _ff = "None"
-        }else if fraudFlag == 1{
+        }else if fraudFlag == 1 as NSNumber{
             _ff = "THIS RECURRING PAYMENT HAS INCREASED"
-        }else if fraudFlag == 2{
+        }else if fraudFlag == 2 as NSNumber{
             _ff = "THIS PAYMENT IS POTENTIALLY FRAUDULENT"
-        }else if fraudFlag == 3{
+        }else if fraudFlag == 3 as NSNumber{
             _ff = "THIS PAYMENT IS LIKELY FRAUDULENT"
         }
         
@@ -219,13 +256,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         msg += cardNum
         msg += " \n " + "Warnings: "
         msg += _ff + " \n "
-        msg += "Fraud Flag: "
-        msg += String(format:"%i", fraudFlag.intValue)
+        //msg += "Fraud Flag: "
+        //msg += String(format:"%i", fraudFlag.intValue)
         let alert = UIAlertController(title: "Transaction Details", message: msg, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true)
+        
+        
     }
     
+    @IBAction func openMenu2(_ sender: Any) {
+        if (menuShowing) {
+            leadingContraint.constant = -150
+            
+            UIView.animate(withDuration: 0.3,
+                           animations: {
+                            self.view.layoutIfNeeded()
+            })
+        }else{
+            leadingContraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3,
+                           animations: {
+                            self.view.layoutIfNeeded()
+            })
+        }
+        
+        menuShowing = !menuShowing
+    }
     
     /*@IBAction func openMenu(_ sender: Any) {
         
@@ -242,7 +300,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         menuShowing = !menuShowing
     }*/
+    
+    var indicator = UIActivityIndicatorView()
+    
+    func activityIndicator() {
+        indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        indicator.style = UIActivityIndicatorView.Style.gray
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+    }
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action:
+            #selector(ViewController.handleRefresh(_:)),
+                                 for: UIControl.Event.valueChanged)
+        refreshControl.tintColor = UIColor.red
+        
+        return refreshControl
+    }()
+    
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        callDtaFromWeb()
+        self.transactionTable.reloadData()
+        refreshControl.endRefreshing()
+    }
 
-
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    
+    @IBOutlet weak var debitCardButtonVar: UIButton!
+    
+    @IBOutlet weak var creditCardButtonVar: UIButton!
+    
+    @IBAction func debitCardButton(_ sender: Any) {
+        creditCardButtonVar.setTitleColor(UIColor.blue, for: [])
+        
+        self.transactions.removeAll()
+        
+        for trans in card1{
+            self.transactions.append(trans)
+        }
+        
+        CARD_TO_USE = "CARD2"
+        
+        self.transactionTable.reloadData()
+        
+        openMenu2(menuButton)
+        
+        debitCardButtonVar.setTitleColor(UIColor.darkGray, for: [])
+    }
+    
+    @IBAction func creditCardButton(_ sender: Any) {
+        debitCardButtonVar.setTitleColor(UIColor.blue, for: [])
+        
+        self.transactions.removeAll()
+        
+        for trans in card2{
+            self.transactions.append(trans)
+        }
+        
+        CARD_TO_USE = "CARD1"
+        
+        self.transactionTable.reloadData()
+        
+        openMenu2(menuButton)
+        
+        creditCardButtonVar.setTitleColor(UIColor.darkGray, for: [])
+    }
 }
 
